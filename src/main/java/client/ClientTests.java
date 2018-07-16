6747e76b8aafc165fa52f2460ce712cfccc0cc8b
Martin Klampfer <e1526110@student.tuwien.ac.at>
2018-07-16 12:02:28 +0200

    better timing and more outputs

--- src/main/java/client/ClientTests.java
+++ src/main/java/client/ClientTests.java
@@ -13,12 +13,17 @@ import java.util.Random;
 
 /**
  * To enable the assertion in the verify method, add the keyword "-ea" to vm options of your IDE
+ *
+ * Erkenntnisse bisher (Stand 16.07.2018)
+ *      Erste Query braucht um einiges länger als folgende
+ *      Erste Query die überhaupt auf GraphQL Server trifft braucht nochmal um vielfaches länger (also erste Query nach Serverstart)
+ *
  */
 public class ClientTests {
 
     private static CloseableHttpClient httpClientForGraphql = null;
     private static String connectionUrl = "http://localhost:8080/graphql";
-    private final static int TESTSIZE = 1000;
+    private final static int TESTSIZE = 500;
 
     private static QueryString queryTomHanks = new QueryString("{\"query\": \"{ getPerson(name: \\\"Tom Hanks\\\") { name born } }\" }", "1956");
     private static QueryString queryRiverPhoenix = new QueryString("{\"query\": \"{ getPerson(name: \\\"River Phoenix\\\") { name born } }\" }", "1970");
@@ -39,37 +44,39 @@ public class ClientTests {
         httpClientForGraphql = HttpClients.createDefault();
         Random rand = new Random();
         long[] avgTimes = new long[TESTSIZE];
-        long minTime = 0;
+        long minTime = Long.MAX_VALUE;
         long maxTime = 0;
+        long maxTimeStartingFromSecondQuery = 0;
 
         long startTime;
         long endTime;
         for (int i = 0; i < TESTSIZE; i++) {
             if (i % 10 == 0) System.out.print(". ");
             int n = rand.nextInt(queries.length);
-            startTime = System.currentTimeMillis();
+            startTime = System.nanoTime();
             requestAndVerify(queries[n].query, queries[n].verification, false);
-            endTime = System.currentTimeMillis();
+            endTime = System.nanoTime();
             avgTimes[i] = endTime - startTime;
             if(avgTimes[i] < minTime) minTime = avgTimes[i];
-            else if(avgTimes[i] > maxTime) maxTime = avgTimes[i];
+            if(avgTimes[i] > maxTime) maxTime = avgTimes[i];
+            if(i != 0 && avgTimes[i] > maxTimeStartingFromSecondQuery) maxTimeStartingFromSecondQuery = avgTimes[i];
         }
+
         System.out.println("\nAll tests done...");
-        long avgTime = avgTimes[0];
+        double avgTime = avgTimes[0] / 1000000;
         for (int i = 1; i < avgTimes.length; i++) {
-            avgTime += avgTimes[i];
+            avgTime += avgTimes[i] / 1000000; //convert nanoseconds to milliseconds
         }
         avgTime /= avgTimes.length;
         System.out.println("\nResults:");
-        System.out.println("Maximum time: "+maxTime);
-        System.out.println("Minimum time: "+ minTime);
+        System.out.println("Maximum time: "+maxTime / 1000000.0);
+        System.out.println("Maximum time after first query: "+maxTimeStartingFromSecondQuery / 1000000.0);
+        System.out.println("Minimum time: "+ minTime / 1000000.0);
         System.out.println("Avg time: "+ avgTime);
     }
 
-
     private static void requestAndVerify(String query, String verifyString, boolean verifyResponse) {
         HttpPost httpPost = new HttpPost(connectionUrl);
-
         try {
             StringEntity params = new StringEntity(query);
 
@@ -80,9 +87,7 @@ public class ClientTests {
             HttpEntity httpEntity = httpResponseFromGraphql.getEntity();
             if (httpEntity != null) {
                 String response = EntityUtils.toString(httpEntity);
-
                 assert !verifyResponse || response.contains(verifyString);
-
                 /*
                 System.out.println("query: " + query);
                 System.out.println("response: " + response);
